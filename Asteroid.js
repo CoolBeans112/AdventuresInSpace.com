6793363196df61ea716d7c6a8a812506292b937b
CoolBeans112 <71298276+CoolBeans112@users.noreply.github.com>
2020-09-28 17:19:05 -0700

    Update Asteroid.js

--- Asteroid.js
+++ Asteroid.js
@@ -5,6 +5,20 @@ this.r = random( 30,50);
 this.move = function() {
 this.pos.add(this.vel);
 }
+  this.edge_loop =function(){
+  if (this.pos.x > width+ this.l) {
+    this.pos.x = -this.l ;
+  }
+  else if (this.pos.x < -this.l) {
+    this.pos.x = width + this.l ;
+  }
+  if (this.pos.y > height+ this.l) {
+    this.pos.y = -this.l ;
+  }
+  else  if (this.pos.y <  -this.l) {
+    this.pos.y = heght +this.l ;
+  }
+}
 this.show = function() {
 ellipse(this.pos.x,this.pos.y, this.r, this.r)
 }
