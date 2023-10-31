e767600162890d95d534b96d938d2ee33034b021
CoolBeans112 <71298276+CoolBeans112@users.noreply.github.com>
2023-10-30 17:48:23 -0700

    Update Sketch.js

--- Sketch.js
+++ Sketch.js
@@ -1,7 +1,10 @@
 let s;
 let lasers = [];
 let stars = [];
-let asteroids = []
+let asteroids = [];
+
+let state = "game";
+
 function setup() {
 let clientHeight = document.getElementById('asteroids').clientHeight;
 let clientWidth = document.getElementById('asteroids').clientWidth;
@@ -19,34 +22,39 @@ s = new Ship ();
 }
 function draw() {
   background(0);
+  if (state == "game") {
   for(let i = 0; i< stars.length; i++){
-    ellipse(stars[i].x, stars[i].y, 4, 4);
-  }
-  s.move();
-  s.turn();
-  s.edge_loop();
-  s.show();
-  for (let i =0; i <asteroids.length; i++){
-  asteroids[i].move();
-  asteroids[i].show();
-  asteroids[i].edge_loop();
- if (asteroids[i].hits(s)) {
- noLoop();
- }
-  }
-for (let i = lasers.length-1; i>=0;i--){
-  lasers[i].update();
-  lasers[i].show();
-for (let JITWT = asteroids.length-1; JITWT >= 0; JITWT--){
-  
-  if (lasers[i].hits(asteroids[JITWT])){
-    asteroids.splice(JITWT, 1);
-    lasers.splice(i, 1);
-    break;
+      ellipse(stars[i].x, stars[i].y, 4, 4);
+    }
+    s.move();
+    s.turn();
+    s.edge_loop();
+    s.show();
+    for (let i =0; i <asteroids.length; i++){
+      asteroids[i].move();
+      asteroids[i].show();
+      asteroids[i].edge_loop();
+      if (asteroids[i].hits(s)) {
+         state = "dead";
+         break;
+      }
+    }
+    for (let i = lasers.length-1; i>=0;i--){
+      lasers[i].update();
+      lasers[i].show();
+      for (let JITWT = asteroids.length-1; JITWT >= 0; JITWT--){
+        if (lasers[i].hits(asteroids[JITWT])){
+          asteroids.splice(JITWT, 1);
+          lasers.splice(i, 1);
+          break;
+        }
+      }
+    }
+  } else if (state == "dead") {
+    background(255, 0, 0);
   }
 }
-}
-}
+
 function keyPressed() {
 if(key == 'w') {
  s.is_boosting = true;
