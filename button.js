83de3f9673d601fcf79c649b678032c3a58260a3
CoolBeans112 <71298276+CoolBeans112@users.noreply.github.com>
2023-11-06 17:59:46 -0800

    Update button.js

--- button.js
+++ button.js
@@ -16,4 +16,7 @@ function Button(x, y, w, h, txt) {
     text(this.txt, this.x, this.y);
   }
   
+  this.collides = function(x, y) {
+    return x > this.x - this.w/2 && x < this.x + this.w/2 && y > this.y - this.h/2 && y < this.y + this.h/2;
+  }
 }
